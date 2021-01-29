e1b79e895bf95d1e445648325a7baf636a0edea3
Divyanshu Agrawal <agrawal.divyanshu@outlook.com>
2021-01-29 18:27:17 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+/*******************/
+
+const {
+  conversation,
+  Image,
+} = require('@assistant/conversation')
+
+// Create an app instance
+const app = conversation()
+
+// Register handlers for Actions SDK
+
+app.handle('handler_name', conv => {
+  conv.add('Hi, how is it going?')
+  conv.add(new Image({
+    url: 'https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/imgs/160204193356-01-cat-500.jpg',
+    alt: 'A cat',
+  }))
+})
+
+/*******************/
+
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .use(bodyParser.json())
+  .post('/fulfillment', app)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
