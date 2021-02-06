d2688ea46704133b7594bd064a8ddd4b6f6d970f
Divyanshu Agrawal <agrawal.divyanshu@outlook.com>
2021-02-07 01:15:54 +0530

    Hard code response as wheat

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Webhook HTTPS Endpoint: 
 // https://agile-island-40140.herokuapp.com/
 
 const express = require("express");
@@ -29,7 +30,7 @@ app.handle("greeting", (conv) => {
 app.handle("crop_information_property_prompt", (conv)=> {
   const crop_name = conv.session.params.crop_name_slot;
   console.log(conv, crop_name);
-  conv.add("OK");
+  conv.add("धान भारत की एक महत्तवपूर्ण फसल है जो कि जोताई योग्य क्षेत्र के लगभग एक चौथाई हिस्से में उगाई जाती है और भारत की लगभग आधी आबादी इसे मुख्य भोजन के रूप में प्रयोग करती है। पिछले 45 वर्षों के दौरान पंजाब ने धान की पैदावार में बहुत ज्यादा उन्नति हासिल की है। नई टैकनोलोजी और अच्छी पैदावार करने वाले बीजों के प्रयोग के कारण धान की पैदावार पंजाब में सबसे ज्यादा होती है। ");
 })
 
 /*******************/
